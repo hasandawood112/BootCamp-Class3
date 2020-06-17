cac7b1b5bfbade4d6f2329f347379a4a6da7d9ea
Hasan Dawood <hasandawood555@gmail.com>
2020-06-18 00:28:51 +0500

    Update marksheet.js

--- src/marksheet.js
+++ src/marksheet.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function Marksheet(props){
+function Marksheet(props) {
 
-    return(
+    return (
         <div>
             <h1>Marksheet</h1>
             <p>English : {props.Eng}</p>
@@ -10,6 +10,10 @@ function Marksheet(props){
             <p>Math : {props.Math}</p>
             <p>Chem : {props.Chem}</p>
             <p>Physics : {props.Physics}</p>
+
+            var total = {props.Eng} + {props.Urdu} + {props.Math} + {props.Chem} + {props.Physics}
+            var per = ({total}/500)*100;
+            <p>Percentage = {per}</p>
 
         </div>
     );
